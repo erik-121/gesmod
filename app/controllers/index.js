890723c8c9d915e3f24f6553fd90b90c0b811d05
Eric Ferrando <eferrando@alfatecsistemas.es>
2019-03-29 13:35:40 +0100

    PrimerCommit

---
+++ app/controllers/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+    index: (req, res, app) => {
+        res.send('Hello, World!');
+    }
+
+}
---
+++ config/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const consign = require('consign');
+
+const app = express();
+
+consign()
+    .include('./app/routes')
+    .then('./app/controllers')
+    .into(app);
+
+module.exports = app;
