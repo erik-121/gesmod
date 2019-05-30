61f97f36743169363fafc30ecfb5df0398fb122a
Eric Ferrando <eferrando@alfatecsistemas.es>
2019-05-30 12:58:52 +0200

    Get Token ok; showAreas improved but not final

--- src/main/java/alfatecsistemas/tdgov/gestionsede/controller/HomeController.java
+++ src/main/java/alfatecsistemas/tdgov/gestionsede/controller/HomeController.java
@@ -1,12 +1,16 @@
 package alfatecsistemas.tdgov.gestionsede.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,69 +18,115 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import alfatecsistemas.tdgov.gestionsede.DemoApplication;
+import alfatecsistemas.tdgov.gestionsede.model.Area;
 import alfatecsistemas.tdgov.gestionsede.model.Token;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping(value = "/showAreas", method = RequestMethod.POST)
-    public String index() {
+	Token tokenAuth;
+
+	@RequestMapping("/token")
+	public void getToken() {
 
 		final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
-		final String URI_acces = "http://172.20.12.101:8888/tdgov/oauth2/token";
+		final String URI_access = "http://172.20.12.101:8888/tdgov/oauth2/token";
 
 		JSONObject obj = new JSONObject();
 
-		obj.put("grant_type","password");
-		obj.put("scope","*");
-		obj.put("client_id","AVLTFROHGPCTDCIQTFAKLZWPDXBHBKAB");
-		obj.put("client_secret","2389151395ce7abe8b950b7015555635");
-		obj.put("username","admin");
-		obj.put("password","Oviedo$2000");
+		obj.put("grant_type", "password");
+		obj.put("scope", "*");
+		obj.put("client_id", "AVLTFROHGPCTDCIQTFAKLZWPDXBHBKAB");
+		obj.put("client_secret", "2389151395ce7abe8b950b7015555635");
+		obj.put("username", "admin");
+		obj.put("password", "Oviedo$2000");
 
 		String body = obj.toString();
-		
+
 		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);		
+		headers.setContentType(MediaType.APPLICATION_JSON);
 
 		HttpEntity<String> myEntity = new HttpEntity<String>(body, headers);
 
 		RestTemplate restTemplate = new RestTemplate();
 
-		ResponseEntity entity = restTemplate.postForEntity(URI_acces, myEntity, String.class);
+		ResponseEntity<String> entity = restTemplate.postForEntity(URI_access, myEntity, String.class);
 
-		if(entity.getStatusCode().value() != 200){
-			log.error("El servidor no responde ");
-			break;
-		}else{
-			List<String> acces_token = entity.getHeaders().get("access_token");
+		if (!entity.getStatusCode().equals(HttpStatus.OK)) {
+			log.error("Problemas con la petición al servidor. Http status:" + entity.getStatusCode());
+
+		} else {
+			String bodyResponse = entity.getBody();
+			String headersResponse = entity.getHeaders().toString();
+			log.info("----Response body----");
+			log.info(bodyResponse);
+			log.info("----Reponse headers----");
+			log.info(headersResponse);
+
+			try {
+				tokenAuth = new ObjectMapper().readValue(bodyResponse, Token.class);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		return acces_token;
 	}
-	
-	@GetMapping(value = "/mostrar")
-    public void getToken() {
+
+	//@RequestMapping()
+	public String getAllAreas2() {
 
 		final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
-		final String URI_acces = "http://172.20.12.101:8888/tdgov/oath2/token";
+		final String URI_access = "http://172.20.12.101:8888/api/1.0/tdgov/project/categories";
 
-		JSONObject obj = new JSONObject();
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + tokenAuth.getAccess_token());
 
-		obj.put("grant_type","password");
-		obj.put("scope","*");
-		obj.put("client_id","AVLTFROHGPCTDCIQTFAKLZWPDXBHBKAB");
-		obj.put("client_secret","2389151395ce7abe8b950b7015555635");
-		obj.put("username","admin");
-		obj.put("password","Oviedo$2000");
+		HttpEntity<String> myEntity = new HttpEntity<String>(headers);
 
-		String body = obj.toString();
+		RestTemplate restTemplate = new RestTemplate();
 
+		// String entity = " KK";
 
-    }
+		try {
+			ResponseEntity<String> entity = restTemplate.getForEntity(URI_access, String.class, myEntity);
+		} catch (RestClientException e) {
+			log.error("Error occurred while getting status of the microservice by URI {}", URI_access, e);
+		}
 
+		String entity = "";
+		return " " + tokenAuth.getAccess_token() + "\n" + entity;
+
+	}
+
+	@RequestMapping(value = "/showAreas", method = RequestMethod.POST)
+	public String getAllAreas() throws IOException {
+
+		OkHttpClient client = new OkHttpClient();
+
+		Request request = new Request.Builder().url("http://172.20.12.101:8888/api/1.0/tdgov/project/categories").get()
+				.addHeader("Authorization", "Bearer " + tokenAuth.getAccess_token())
+				.addHeader("User-Agent", "PostmanRuntime/7.13.0").addHeader("Accept", "*/*")
+				.addHeader("Cache-Control", "no-cache")
+				.addHeader("Postman-Token", "76c2c46a-4454-491d-8f1e-e69f791230e4,79f69674-af82-4647-b852-2d760ffad500")
+				.addHeader("Host", "172.20.12.101:8888").addHeader("accept-encoding", "gzip, deflate")
+				.addHeader("Connection", "keep-alive").addHeader("cache-control", "no-cache").build();
+
+		Response response = client.newCall(request).execute();
+		return response.body().string();
+
+		//Area areas = new ObjectMapper().readValue(response.body().string(), Area.class);
+
+		//JSONObject jsonObj = new JSONObject(response.body().string());
+
+		//return jsonObj;
+	}
 }
--- src/main/java/alfatecsistemas/tdgov/gestionsede/model/Token.java
+++ src/main/java/alfatecsistemas/tdgov/gestionsede/model/Token.java
@@ -1,24 +1,22 @@
 package alfatecsistemas.tdgov.gestionsede.model;
 
-
-
 public class Token{
 
-    private Long access_token;
+    private String access_token;
     private int expires_in;
     private String token_type;
     private String scope;
-    private Long refresh_token;
+    private String refresh_token;
 
-    public Long getAccess_token() {
+    public String getAccess_token() {
         return access_token;
     }
 
-    public Long getRefresh_token() {
+    public String getRefresh_token() {
         return refresh_token;
     }
 
-    public void setRefresh_token(Long refresh_token) {
+    public void setRefresh_token(String refresh_token) {
         this.refresh_token = refresh_token;
     }
 
@@ -46,7 +44,7 @@ public class Token{
         this.expires_in = expires_in;
     }
 
-    public void setAccess_token(Long access_token) {
+    public void setAccess_token(String access_token) {
         this.access_token = access_token;
     }
 
