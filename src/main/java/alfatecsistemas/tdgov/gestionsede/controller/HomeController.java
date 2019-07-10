531b59102edc10654c2d2b0c1876a19d603a1ca9
Eric Ferrando <eferrando@alfatecsistemas.es>
2019-07-10 11:51:16 +0200

    create new Category function

--- src/main/java/alfatecsistemas/tdgov/gestionsede/controller/HomeController.java
+++ src/main/java/alfatecsistemas/tdgov/gestionsede/controller/HomeController.java
@@ -3,23 +3,35 @@ package alfatecsistemas.tdgov.gestionsede.controller;
 import java.io.IOException;
 import com.google.gson.Gson;
 
+import org.apache.commons.logging.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
 import alfatecsistemas.tdgov.gestionsede.model.Token;
+import alfatecsistemas.tdgov.gestionsede.DemoApplication;
 import alfatecsistemas.tdgov.gestionsede.model.Category;
 import alfatecsistemas.tdgov.gestionsede.repository.TokenRepositoryImpl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+
 @Controller
 public class HomeController {
+
+	final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static Token tokenId;
 
@@ -81,4 +93,33 @@ public class HomeController {
 		return "listAreas";
 
 	}
+	@PostMapping(value = "/newArea")
+	public String newArea(Model model,@RequestParam(name = "newAreaName")String newArea) throws IOException {
+
+		Category newOne = new Category();
+		newOne.setCatName(newArea);
+
+		Gson gson = new Gson();
+		String json = gson.toJson(newOne);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.add("Authorization", "Bearer " + tokenId.getAccess_token());
+		
+		HttpEntity<String> myEntity = new HttpEntity<String>(json, headers);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		try {
+			ResponseEntity<String> entity = restTemplate.postForEntity("http://"+serverIP+":"+serverPort+"/api/1.0/"+workspace+"/project/category", myEntity, String.class);
+		} catch (RestClientException  e) {
+			log.error("Error:  " + e.toString());
+		}
+		
+		log.info("User ordered to create new Area: " + newArea);
+
+		return "login";
+
+    }
+	
 }
