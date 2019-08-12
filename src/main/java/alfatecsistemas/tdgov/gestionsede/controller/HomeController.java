eeba91d9cabb9b91018bd4d3314c45e3f616645b
Eric Ferrando <eferrando@alfatecsistemas.es>
2019-08-12 13:43:27 +0200

    initial commit2

--- src/main/java/alfatecsistemas/tdgov/gestionsede/controller/HomeController.java
+++ src/main/java/alfatecsistemas/tdgov/gestionsede/controller/HomeController.java
@@ -1,25 +1,41 @@
 package alfatecsistemas.tdgov.gestionsede.controller;
 
 import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.google.gson.Gson;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
 import alfatecsistemas.tdgov.gestionsede.model.Token;
+import alfatecsistemas.tdgov.gestionsede.DemoApplication;
 import alfatecsistemas.tdgov.gestionsede.model.Category;
 import alfatecsistemas.tdgov.gestionsede.repository.TokenRepositoryImpl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+
 @Controller
 public class HomeController {
+
+	final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static Token tokenId;
 
@@ -53,7 +69,7 @@ public class HomeController {
 	 * @throws IOException
 	 */
 	@PostMapping(value = "/showAreas")
-	public String getAllAreas(Model model, @RequestParam(name = "user") String user) throws IOException {
+	public String getAllAreas(Model model, @RequestParam(name = "user", required = false) String user) throws IOException {
 
 		TokenRepositoryImpl token = new TokenRepositoryImpl();
 		tokenId = token.getToken();
@@ -81,4 +97,65 @@ public class HomeController {
 		return "listAreas";
 
 	}
+	@PostMapping(value = "/newCategory")
+	public String newArea(Model model,@RequestParam(name = "newAreaName")String newCategory) throws IOException {
+
+		Category newOne = new Category();
+		newOne.setCatName(newCategory);
+
+		Gson gson = new Gson();
+		String json = gson.toJson(newOne);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.add("Authorization", "Bearer " + tokenId.getAccess_token());
+		
+		HttpEntity<String> myEntity = new HttpEntity<String>(json, headers);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		try {
+			ResponseEntity<String> entity = restTemplate.postForEntity("http://"+serverIP+":"+serverPort+"/api/1.0/"+workspace+"/project/category", myEntity, String.class);
+		} catch (RestClientException  e) {
+			log.error("Error:  " + e.toString());
+		}
+		
+		log.info("User ordered to create new Area: " + newCategory);
+
+		return "redirect:/";
+
+	}
+	
+	@ResponseBody
+	@PostMapping(params = "removeCategory", path = {"/actions/delete", "/actions/delete/{id}"})
+    public void removeCategory(@RequestParam("removeCategory") String index, HttpServletRequest request) {
+
+		String body = null;
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + tokenId.getAccess_token());
+		HttpEntity<String> myEntity = new HttpEntity<String>(headers);
+		RestTemplate restTemplate = new RestTemplate();
+
+		try {
+			restTemplate.exchange("http://"+serverIP+":"+serverPort+"/api/1.0/"+workspace+"/project/category/"+index, HttpMethod.DELETE, myEntity, String.class);
+			log.info("Borrada la categoría con UID:"+index);
+		} catch (RestClientException e) {
+			log.error("Error:  " + e.toString());
+		}
+        //order.items.remove(index);
+        /*if (AJAX_HEADER_VALUE.equals(request.getHeader(AJAX_HEADER_NAME))) {
+            return "order::#items";
+        } else {
+            return "order";
+        }*/
+	}
+	
+	//@ResponseBody
+	@GetMapping(params = "editCategory", path = {"/actions/edit", "actions/edit/{id}"})
+	public String editCategory(Model model, @RequestParam("editCategory") String id, HttpServletRequest request) {
+
+
+		return "editCategory";
+	}
+	
 }
